06bff710f10ad344ceaab2ba1283b8a1a728ca8d
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-11 23:29:48 +0530

    not

--- pattern.js
+++ pattern.js
@@ -24,5 +24,15 @@ for (let i = 1; i <= n; i++) {
   console.log(str);
 }
 console.log('*'.repeat(2 * n - 1));  // Base of the pyramid
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let str = ' '.repeat(n - i - 1);
+  let num = 1;
+  for (let j = 0; j <= i; j++) {
+    str += num + ' ';
+    num = num * (i - j) / (j + 1); // Calculate the next value in the row
+  }
+  console.log(str);
+}
 
 
