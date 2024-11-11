276460c6ace277ea663c774f46bb115731e60848
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-11 23:30:43 +0530

    hhh

--- pattern.js
+++ pattern.js
@@ -34,5 +34,13 @@ for (let i = 0; i < n; i++) {
   }
   console.log(str);
 }
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  console.log(' '.repeat(n - i) + '*' + ' '.repeat(2 * (i - 1) - 1) + (i === 1 ? '' : '*'));
+}
+for (let i = n - 1; i >= 1; i--) {
+  console.log(' '.repeat(n - i) + '*' + ' '.repeat(2 * (i - 1) - 1) + (i === 1 ? '' : '*'));
+}
+
 
 
