3cbcde3a31599c57780d8c3cddacec67e86b7c7d
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-11 23:27:12 +0530

    kjjbb

--- pattern.js
+++ pattern.js
@@ -3,3 +3,7 @@ for (let i = 1; i <= n; i++) {
   let str = ' '.repeat(n - i) + '*'.repeat(2 * i - 1);
   console.log(str);
 }
+let n = 5;
+for (let i = 1; i <= n; i++) {
+  console.log('*'.repeat(i));
+}
