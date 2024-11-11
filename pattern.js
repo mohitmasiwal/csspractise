5f403ac6b6e93faff1aba81795516a21e059ba3f
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-11 23:32:13 +0530

    nnn

--- pattern.js
+++ pattern.js
@@ -39,7 +39,7 @@ for (let i = 1; i <= n; i++) {
   console.log(' '.repeat(n - i) + '*' + ' '.repeat(2 * (i - 1) - 1) + (i === 1 ? '' : '*'));
 }
 for (let i = n - 1; i >= 1; i--) {
-  console.log(' '.repeat(n - i) + '*' + ' '.repeat(2 * (i - 1) - 1) + (i === 1 ? '' : '*'));
+  console.log(' '.repeat(n - i) + '*==' + ' '.repeat(2 * (i - 1) - 1) + (i === 1 ? '' : '*'));
 }
 
 
