19cc0f6a3a810d67f93da6c2da8ba00978ca5ecf
mohitmasiwal <mohitmasiwal02@gmail.com>
2024-11-12 12:26:49 +0530

    new comiopt

--- ternaryoperator/to.js
+++ ternaryoperator/to.js
@@ -1,9 +1,9 @@
  num = 444;
  num < 100? console.log("less"):console.log("more");
 
- const great =( delf)=>{
+ const great =( delf="enter name")=>{ /// defaultr value used whem no parametwr is pased 
     console.log ("hello mate "+ delf );
    
  }
  
- great( "mohit")
+ great( )
